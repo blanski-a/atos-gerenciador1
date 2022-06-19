025017650a93386f7676b146e61a199055b2ca0a
blanski-a <71938996+blanski-a@users.noreply.github.com>
2022-06-19 17:54:35 -0300

    primeiro commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <footer className="main-footer">
+            <strong> <a href="http://adminlte.io"></a>.</strong>
+            
+            <div className="float-right d-none d-sm-inline-block">
+                <b>Version</b> 3.0.5
+            </div>
+        </footer>
+     );
+}
+ 
+export default Footer;
